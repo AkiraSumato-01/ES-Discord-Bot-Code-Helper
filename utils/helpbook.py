3ce670e34c22e81234db4461df81ed2f57200e00
MackDillan <mackdillan212@gmail.com>
2018-10-02 19:50:59 +0300

    FIX FIX 2

--- utils/helpbook.py
+++ utils/helpbook.py
@@ -9,9 +9,12 @@ available_tags = [x[0:-4] for x in os.listdir('info/')]
 available_tags.sort()
 
 for i in range(len(available_tags)):
-    available_tags[i] =str(i+1) +' '+ available_tags[i]
+    available_tags[i] = str(i+1) +' '+ available_tags[i]
 
 def content(tag):
     if tag.isdigit():
         tag = available_tags[int(tag) - 1]
+        tag = tag.split(' ', 1)
+        tag = tag[1]
+        tag = tag[:-4]
     return io.open('info/' + tag + '.txt', 'r', encoding='utf-8').read()
