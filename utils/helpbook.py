f8c66c32047aa4e5effe371b26779cb5a0781bc9
MackDillan <mackdillan212@gmail.com>
2018-10-02 21:16:52 +0300

    add sort and digit list

--- utils/helpbook.py
+++ utils/helpbook.py
@@ -6,6 +6,18 @@ import io
 
 available_tags = [x[0:-4] for x in os.listdir('info/')]
 
+available_tags.sort()
+
+available_tags_dig_list = list()
+
+for i in range(len(available_tags):
+    lst_apnd = str(i+1) + available_tags[i]
+    available_tags_dig_list.append(lst_apnd)
+
+del x
+del i
+del lst_apnd
 
 def content(tag):
+    if tag.isdigit():
     return io.open('info/' + tag + '.txt', 'r', encoding='utf-8').read()
