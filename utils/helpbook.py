f51f6466251a72af29d04f656dd26585886ba2ae
MackDillan <mackdillan212@gmail.com>
2018-10-02 20:29:50 +0300

    fix sort

--- utils/helpbook.py
+++ utils/helpbook.py
@@ -5,6 +5,8 @@ import os
 import io
 
 available_tags = [x[0:-4] for x in os.listdir('info/')]
+
 available_tags.sort()
+
 def content(tag):
     return io.open('info/' + tag + '.txt', 'r', encoding='utf-8').read()
