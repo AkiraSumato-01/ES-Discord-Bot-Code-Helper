562678b742a8d5c5435ff3e5517bd84041afefa2
AkiraSumato_01 <minewindowsteam@yandex.ru>
2018-10-02 21:49:27 +0400

    Some changes

--- cogs/help.py
+++ cogs/help.py
@@ -59,7 +59,7 @@ class Help(object):
         list_tags = "\n".join(help_.available_tags.sort())
 
         if tag not in help_.available_tags:
-            return await ctx.send(f'Доступные теги: \n```{list_tags}```\n %s'%len(help_.available_tags))
+            return await ctx.send(f'Доступные теги: \n```{list_tags}```\n\n{len(help_.available_tags)}')
         
         await ctx.send(embed=discord.Embed(timestamp=ctx.message.created_at,
                                            color=randint(0x000000, 0xFFFFFF),
