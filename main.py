290856091c174b2fa34897d92cd9e5a9d0f7b5d1
AkiraSumato-01 <40606774+AkiraSumato-01@users.noreply.github.com>
2019-03-01 18:06:43 +0700

    Add files via upload

--- main.py
+++ main.py
@@ -18,7 +18,7 @@ from discord.ext import commands
 bot = commands.Bot(command_prefix='?')
 bot.remove_command('help')
 
-extensions = ['cogs.help',
+extensions = ['cogs.help', 'cogs.emergency',
               'cogs.owner',
               'cogs.default',
               'cogs.error_handler']
